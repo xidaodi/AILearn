185a37933d4ee5b9424641cc38fdf2a83011f5d9
xidaodi <35069982+xidaodi@users.noreply.github.com>
2021-02-22 10:08:41 +0800

    2021.02.22 10:08 PM

--- kaikeba/AIPython基础/main.py
+++ kaikeba/AIPython基础/main.py
@@ -383,28 +383,28 @@ varstr="Iloveyou123 &loveu521ove2love234567"
 # print(res)
 
 # 自定义异常日志处理类
-class Myexception():
-    def __init__(self):
-        import traceback
-        import logging
-
-        # logging的基本配置
-        logging.basicConfig(
-            filename='./error.log',# 日志存储的文件及目录
-            format='%(asctime)s  %(levelname)s \n %(message)s',# 格式化存储的日志格式
-            datefmt='%Y-%m-%d %H:%M:%S'
-        )
-        # 写入日志
-        logging.error(traceback.format_exc())
-
-# 使用自定义异常处理类
-try:
-    int('bb')
-except:
-    print('在此处进行异常的处理')
-    Myexception() # 在异常处理的代码块中去调用自定义异常类
-
-print('abcdef')
+# class Myexception():
+#     def __init__(self):
+#         import traceback
+#         import logging
+#
+#         # logging的基本配置
+#         logging.basicConfig(
+#             filename='./error.log',# 日志存储的文件及目录
+#             format='%(asctime)s  %(levelname)s \n %(message)s',# 格式化存储的日志格式
+#             datefmt='%Y-%m-%d %H:%M:%S'
+#         )
+#         # 写入日志
+#         logging.error(traceback.format_exc())
+#
+# # 使用自定义异常处理类
+# try:
+#     int('bb')
+# except:
+#     print('在此处进行异常的处理')
+#     Myexception() # 在异常处理的代码块中去调用自定义异常类
+#
+# print('abcdef')
 
 
 
